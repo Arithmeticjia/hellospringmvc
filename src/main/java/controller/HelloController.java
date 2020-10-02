9361d6a293414d9c69c7a7c61b6d2c87154427f9
ArithmeticJia <1097197237@qq.com>
2020-10-02 17:50:18 +0800

    sth

--- src/main/java/controller/HelloController.java
+++ src/main/java/controller/HelloController.java
@@ -1,11 +1,15 @@
 package controller;
 
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.stereotype.Controller;
 
-public class HelloController implements Controller {
 
-    @Override
+@Controller
+public class HelloController{
+
+    @RequestMapping("/hello")
     public ModelAndView handleRequest(javax.servlet.http.HttpServletRequest httpServletRequest, javax.servlet.http.HttpServletResponse httpServletResponse) throws Exception {
         ModelAndView mav = new ModelAndView("index.jsp");
         mav.addObject("message", "Hello Spring MVC");
